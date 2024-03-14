4330f01c450a4555a7b179f4f43c02d577ee8cee
sanhorva <161876309+sanhorva@users.noreply.github.com>
2024-03-14 21:15:02 +0100

    Update main.tf

--- Modules/StorageAccount/main.tf
+++ Modules/StorageAccount/main.tf
@@ -8,16 +8,14 @@ terraform {
   }
 }
 
-resource "random_string" "random" {
-  length = 6
-  special = false
-  upper = false
-
-  
-}
+#resource "random_string" "random" {
+#  length = 6
+#  special = false
+#  upper = false
+#}
 
 resource "azurerm_storage_account" "sa-sanyi" {
-  name                     =  "${lower(var.sa_name)}${random_string.random.result}"
+  name                     =  "${lower(var.sa_name)}" # ${random_string.random.result}"
   resource_group_name      = var.rg_name
   location                 = var.rg_location
   account_tier             = "Standard"
@@ -27,7 +25,6 @@ resource "azurerm_storage_account" "sa-sanyi" {
     environment = "staging"
   }
 }
-
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
