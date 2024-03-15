147f98a0e3c962d8bb92f09dece78d0792cd461c
sanhorva <161876309+sanhorva@users.noreply.github.com>
2024-03-15 14:22:11 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "stg_name" {
   value = module.StorageAccount.out.name
 }
 
-output "rg_name" {
-  value = azurerm_resource_group.rg-sanhorva.name
-}
-
 output "azure_subnet_id" {
   value = module.subnet.azure_subnet_id
 }
